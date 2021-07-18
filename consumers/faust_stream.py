c61724421c9c08a9327ef40f69578bd4b4ce7434
Wisnu Mulya <wisnu@wisnumulya.com>
2021-07-19 01:54:47 +0700

    Created stream processing with Faust

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -31,18 +31,18 @@ class TransformedStation(faust.Record):
 
 # TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
 #   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
+app = faust.App("cta.transform.stations", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("cta.producer.stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("org.chicago.cta.stations.table.v1", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   'stations.transformed',
+   default=int,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
@@ -52,6 +52,26 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
 #
 #
+@app.agent(topic)
+async def transform_stations(stations):
+    async for station in stations:
+        color = None
+        if station.red:
+            color = 'red'
+        elif station.blue:
+            color = 'blue'
+        else:
+            color = 'green'
+            
+        station_transformed = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=color
+        )
+        
+        # Keep the latest data on stations
+        table[station.station_id] = station_transformed
 
 
 if __name__ == "__main__":
